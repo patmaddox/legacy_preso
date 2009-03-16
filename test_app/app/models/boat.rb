9223babc08b4dc733419a0c6cca08ef6c477e18f
BJ Clark <bj@aboutus.org>
2009-03-15 17:01:47 -0700

    flatten some code

--- test_app/app/models/boat.rb
+++ test_app/app/models/boat.rb
@@ -1,13 +1,7 @@
 class Boat < ActiveRecord::Base
   belongs_to :owner, :class_name => "User"
-  
+
   def can_update?(user)
-    user == owner ||
-      user.admin? ||
-      !claimed?
-  end
-  
-  def claimed?
-    owner
+    user == owner || user.admin? || owner.blank?
   end
 end
